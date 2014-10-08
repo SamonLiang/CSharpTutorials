ef1d31a37ff122f9d55e61815b4b6f463a5d45ab
SamonLiang <13713878410@163.com>
2014-10-08 21:36:14 +0800

    Create unit test project

---
+++ UnitTestWithFakesFramework/Src/FileSystemUtility.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace CSharpTutorials.UnitTestWithFakesFramework
+{
+    public static class FileSystemUtility
+    {
+        public static bool FileExists(string filePath)
+        {
+            return File.Exists(filePath);
+        }
+    }
+}
